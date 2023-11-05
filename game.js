05851b3f5b83b8bbb7f07c3b8da7de5cfa330996
Ramona Fuchs <65505215+raexune@users.noreply.github.com>
2023-11-06 00:40:47 +0100

    2. commit

--- game.js
+++ game.js
@@ -2,6 +2,8 @@
 
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
+const questionCounterText = document.getElementById("questionCounter");
+const scoreText = document.getElementById("score");
 
 let currentQuestion = {};
 let acceptingAnswer = false;
@@ -29,10 +31,10 @@ let questions = [
   },
   {
     question: "How do you write 'Hello World!' in an alert box?",
-    choice1: "msgBox('Hello World?')",
-    choice2: "alertBox('Hello World?')",
-    choice3: "msg('Hello World?')",
-    choice4: "alert('Hello World?')",
+    choice1: "msgBox('Hello World!')",
+    choice2: "alertBox('Hello World!')",
+    choice3: "msg('Hello World!')",
+    choice4: "alert('Hello World!')",
     answer: 4
   }
 ];
@@ -50,7 +52,13 @@ startGame = () => {
 };
 
 getNewQuestions = () => {
+  if (availibleQuestions == 0 || questionCounter > MAX_QUESTIONS) {
+    localStorage.setItem("mostRecentScore", score);
+    return window.location.assign("/end.html");
+  }
   questionCounter++;
+  questionCounterText.innerText = questionCounter + "/" + MAX_QUESTIONS;
+
   const questionIndex = Math.floor(Math.random() * availibleQuestions.length);
   currentQuestion = availibleQuestions[questionIndex];
   question.innerText = currentQuestion.question;
@@ -67,12 +75,35 @@ getNewQuestions = () => {
 
 choices.forEach((choice) => {
   choice.addEventListener("click", (e) => {
-    if (acceptingAnswer) return;
+    if (!acceptingAnswer) return;
 
     acceptingAnswer = false;
     const selectedChoice = e.target;
-    const selectedAnswer = selectedChoice.dataset("number");
+    const selectedAnswer = selectedChoice.dataset["number"];
+
+    let classToAply = "incorrect";
+    if (selectedAnswer == currentQuestion.answer) {
+      classToAply = "correct";
+    }
+
+    if (classToAply == "correct") {
+      incrementScore(CORRECT_BONUS);
+    }
+    console.log(classToAply);
+
+    console.log(selectedAnswer == currentQuestion);
+
+    selectedChoice.parentElement.classList.add(classToAply);
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToAply);
+      getNewQuestions();
+    }, 1000);
   });
 });
+
+incrementScore = (num) => {
+  score += num;
+  scoreText.innerText = score;
+};
 
 startGame();
